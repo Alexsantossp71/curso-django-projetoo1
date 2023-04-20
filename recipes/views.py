7c35ede196121edfab27945c6a657675efa5f10d
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2023-04-20 18:55:35 -0300

    com o style

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Alexandre'})
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
