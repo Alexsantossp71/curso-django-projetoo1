a7e78c4b801bfa045c277938e3a3a26275444349
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2023-04-27 16:37:53 -0300

    aula 61

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Recipe, RecipeAdmin)
